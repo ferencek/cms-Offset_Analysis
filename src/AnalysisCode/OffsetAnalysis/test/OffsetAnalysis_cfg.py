f841d8cd57b74b92be620b2625fa08f8e3508edc
ferencek <>
2009-10-02 12:07:53 +0000

    files for the October 09 Exercise

---
+++ src/AnalysisCode/OffsetAnalysis/test/OffsetAnalysis_cfg.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("USER")
+
+process.load('Geometry.CMSCommonData.cmsIdealGeometryXML_cfi')
+process.load('Geometry.CaloEventSetup.CaloGeometry_cff')
+process.load('Geometry.CaloEventSetup.CaloTowerConstituents_cfi')
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 500
+process.MessageLogger.cerr.default.limit = 10
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:input_file.root')
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('OffsetAnalysis_histos.root')
+)
+
+process.offsetAnalysis = cms.EDAnalyzer("OffsetAnalysis",
+    doOffset = cms.untracked.bool(True),
+    towerETcut = cms.untracked.double(0.5),
+    #caloTowers = cms.untracked.string('towerMaker'),
+    loopOverRecHits = cms.untracked.bool(False),
+    recHitEvtLimit = cms.untracked.int32(2000),
+    doPVs = cms.untracked.bool(False),
+    maxNPVs = cms.untracked.int32(5),
+    pvCollection = cms.InputTag('offlinePrimaryVertices')
+)
+
+# Path definition
+process.p = cms.Path(process.offsetAnalysis)
+process.schedule = cms.Schedule(process.p)
